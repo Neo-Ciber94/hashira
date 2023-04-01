38cbe3dcf3b912f75dba76f2ebed43b046fdf1fc
Freddy L <neo-ciber_94@hotmail.com>
2023-04-01 13:50:28 -0400

    Updated markers

--- packages/hashira/src/components/markers.rs
+++ packages/hashira/src/components/markers.rs
@@ -1,36 +1,30 @@
 // TODO: append an uuid to ensure the value is unique?
 
-pub const HASHIRA_META_MARKER: &str = "__hashira__meta__";
-pub const HASHIRA_CONTENT_MARKER: &str = "__hashira__content__";
-pub const HASHIRA_LINKS_MARKER: &str = "__hashira__links__";
-pub const HASHIRA_SCRIPTS_MARKER: &str = "__hashira__scripts__";
+use yew::AttrValue;
+
+pub const HASHIRA_META_MARKER: &str = "<!--hashira_meta-->";
+pub const HASHIRA_CONTENT_MARKER: &str = "<!--hashira_content-->";
+pub const HASHIRA_LINKS_MARKER: &str = "<!--hashira_links-->";
+pub const HASHIRA_SCRIPTS_MARKER: &str = "<!--hashira_scripts-->";
 pub const HASHIRA_ROOT: &str = "__hashira__root__";
 pub const HASHIRA_PAGE_DATA: &str = "__hashira__page_data__";
 
 #[yew::function_component]
 pub fn Meta() -> yew::Html {
-    return yew::html! {
-        format!("{HASHIRA_META_MARKER}\n")
-    };
+    yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_META_MARKER))
 }
 
 #[yew::function_component]
 pub fn Content() -> yew::Html {
-    return yew::html! {
-        format!("{HASHIRA_CONTENT_MARKER}\n")
-    };
+    yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_CONTENT_MARKER))
 }
 
 #[yew::function_component]
 pub fn Links() -> yew::Html {
-    return yew::html! {
-        format!("{HASHIRA_LINKS_MARKER}\n")
-    };
+    yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_LINKS_MARKER))
 }
 
 #[yew::function_component]
 pub fn Scripts() -> yew::Html {
-    return yew::html! {
-        format!("{HASHIRA_SCRIPTS_MARKER}\n")
-    };
+    yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_SCRIPTS_MARKER))
 }
