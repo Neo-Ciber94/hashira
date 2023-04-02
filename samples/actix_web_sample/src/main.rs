947a9e3b198ac1384509cf42999f62a5115a2589
Freddy L <neo-ciber_94@hotmail.com>
2023-04-02 00:25:05 -0400

    minor changes

--- samples/actix_web_sample/src/main.rs
+++ samples/actix_web_sample/src/main.rs
@@ -1,18 +1,24 @@
 mod components;
 use components::App;
 
+#[cfg(not(target_arch = "wasm32"))]
+mod index;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod server;
+
+#[cfg(target_arch = "wasm32")]
+mod client;
+
 cfg_if::cfg_if! {
     if #[cfg(not(target_arch = "wasm32"))] {
-        mod index;
-        mod server;
-
         // Generate index.html
         #[cfg(feature = "build")]
         #[actix_web::main]
         async fn main() {
             crate::index::generate_html_index::<App>().await;
         }
-        
+
         // Server
         #[cfg(not(feature = "build"))]
         #[actix_web::main]
@@ -21,8 +27,6 @@ cfg_if::cfg_if! {
         }
 
 } else {
-        mod client;
-
         // Client
         fn main() {
             crate::client::start_client::<App>();
