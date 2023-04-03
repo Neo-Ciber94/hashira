e1705ac77fba26a31db6f27093f8000de7881281
Freddy L <neo-ciber_94@hotmail.com>
2023-04-03 18:50:03 -0400

    docs

--- packages/hashira/src/components/root.rs
+++ packages/hashira/src/components/root.rs
@@ -2,6 +2,9 @@ use crate::components::{Content, Links, Main, Meta, Scripts};
 use yew::{function_component, Html};
 
 /// Default root component.
+/// 
+/// This element only is rendered on the server side and defines the overall
+/// structure of the html where the page will be rendered.
 #[function_component]
 pub fn RootLayout() -> Html {
     yew::html! {
