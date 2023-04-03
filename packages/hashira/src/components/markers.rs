ae60a8030542d75529cc15e4701403b7e6eb641d
Freddy L <neo-ciber_94@hotmail.com>
2023-04-03 18:48:23 -0400

    Added comments to markers

--- packages/hashira/src/components/markers.rs
+++ packages/hashira/src/components/markers.rs
@@ -1,6 +1,4 @@
-// TODO: append an uuid to ensure the value is unique?
-
-use yew::AttrValue;
+use yew::{html::ChildrenProps, AttrValue};
 
 pub const HASHIRA_META_MARKER: &str = "<!--hashira_meta-->";
 pub const HASHIRA_CONTENT_MARKER: &str = "<!--hashira_content-->";
@@ -9,22 +7,36 @@ pub const HASHIRA_SCRIPTS_MARKER: &str = "<!--hashira_scripts-->";
 pub const HASHIRA_ROOT: &str = "__hashira__root__";
 pub const HASHIRA_PAGE_DATA: &str = "__hashira__page_data__";
 
+/// A marker for insert the page `<meta>` elements.
 #[yew::function_component]
 pub fn Meta() -> yew::Html {
     yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_META_MARKER))
 }
 
+/// A marker for insert the page component.
 #[yew::function_component]
 pub fn Content() -> yew::Html {
     yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_CONTENT_MARKER))
 }
 
+/// A marker for insert the page `<link>` elements.
 #[yew::function_component]
 pub fn Links() -> yew::Html {
     yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_LINKS_MARKER))
 }
 
+/// A marker for insert the page `<script>` elements.
 #[yew::function_component]
 pub fn Scripts() -> yew::Html {
     yew::Html::from_html_unchecked(AttrValue::from(HASHIRA_SCRIPTS_MARKER))
+}
+
+/// A components that insert a `<main>` element where the page will be rendered and hydrated.
+#[yew::function_component]
+pub fn Main(props: &ChildrenProps) -> yew::Html {
+    yew::html! {
+        <main id={HASHIRA_ROOT}>
+            {for props.children.iter()}
+        </main>
+    }
 }
--- packages/hashira/src/components/root.rs
+++ packages/hashira/src/components/root.rs
@@ -1,20 +1,21 @@
-use crate::components::{Content, Links, Meta, Scripts, HASHIRA_ROOT};
+use crate::components::{Content, Links, Main, Meta, Scripts};
 use yew::{function_component, Html};
 
 /// Default root component.
 #[function_component]
 pub fn RootLayout() -> Html {
     yew::html! {
-        <html lang={"en"}>
+        <html lang="en">
             <head>
                 <Meta/>
                 <Links/>
+                <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </head>
             <body>
-                <main id={HASHIRA_ROOT}>
+                <Main>
                     <Content/>
-                </main>
+                </Main>
                 <Scripts/>
             </body>
         </html>
