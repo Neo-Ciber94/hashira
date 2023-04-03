c3afc2335d6ac380e243ce022e5b083ffa81903f
Freddy L <neo-ciber_94@hotmail.com>
2023-04-03 18:54:37 -0400

    docs

--- packages/hashira/src/web/mod.rs
+++ packages/hashira/src/web/mod.rs
@@ -7,10 +7,13 @@ mod request_ext;
 pub use response_ext::*;
 pub use request_ext::*;
 
+/// The body of a request/response.
 pub type Body = bytes::Bytes;
 
+/// Represents a `http` request.
 pub type Request<T = Body> = http::request::Request<T>;
 
+/// Represents a `http` response.
 pub type Response<T = Body> = http::response::Response<T>;
 
 pub use http::header;
