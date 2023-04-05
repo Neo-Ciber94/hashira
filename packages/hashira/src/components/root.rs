79e16e9517f1d01d5989cfb3bc234c1539102b68
Freddy L <neo-ciber_94@hotmail.com>
2023-04-05 01:28:44 -0400

    docs

--- packages/hashira/src/components/root.rs
+++ packages/hashira/src/components/root.rs
@@ -8,17 +8,31 @@ use yew::{function_component, Html};
 #[function_component]
 pub fn RootLayout() -> Html {
     yew::html! {
+        // Base <html>
         <html lang="en">
+            // All the <head> elements
             <head>
+                // A marker for <title> of the page
                 <Title/>
+
+                // A marker for the <meta> elements of the page
                 <Meta/>
+
+                // A marker for the <link> elements of the page
                 <Links/>
                 <meta charset="utf-8" />
             </head>
+
+            // The <body>
             <body>
+                // Renders a <main> element where the page will be rendered and hydrated
                 <Main>
+                    // Were the page will be inserted after rendered
                     <Content/>
                 </Main>
+
+                // Adds the page <script> elements and also the script to hydrate the page
+                // to provide interactivity
                 <Scripts/>
             </body>
         </html>
