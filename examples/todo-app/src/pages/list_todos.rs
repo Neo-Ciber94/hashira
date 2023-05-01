1b8b8f6e303b40f82a2101449f9ec9a02b4cd821
Freddy L <neo-ciber_94@hotmail.com>
2023-05-01 16:02:46 -0400

    minor changes

--- examples/todo-app/src/pages/list_todos.rs
+++ examples/todo-app/src/pages/list_todos.rs
@@ -1,6 +1,11 @@
 use hashira::page_component;
 
+// pub fn GetText(ctx: RenderContext) -> Vec<String> {
+//     todo!()
+// }
+
 #[page_component]
+//#[hashira::loader(GetText)]
 pub fn ListTodosPage() -> yew::Html {
     yew::html! {
         "List Todos Page"
